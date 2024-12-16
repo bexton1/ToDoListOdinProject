72c77adf188c863c7641aa83e73e6a6b43984530
bexton1 <bfe1395@gmail.com>
2024-12-16 12:27:27 +1100

    fix flexbox issues

--- src/loadhomepage.js
+++ src/loadhomepage.js
@@ -10,25 +10,30 @@ mainContainer.innerHTML = `    <h2>Inbox</h2>
                 <p id="add-task-main">Add Task</p>
             </div>
            
-            <div class="main-add-task">
+           
                 
                 <div class="add-task-module active">
                     <form id="form">
                         <input type="text" name="" id="todo-name" placeholder="ToDo Name">
                         <textarea name="" id="description" placeholder="description"></textarea>
-                        <input type="date" name="" id="date">
+                        <div class='flex-container'>
+                        <div>
+                         <input type="date" name="" id="date">
                         <select name="" id="priority">
                             <option value="priority 1">priority 1</option>
                             <option value="priority 2">priority 2</option>
                             <option value="priority 3">priority 3</option>
                         </select>
+                        </div>
+                       
                         
                         <div class="task-buttons">
                             <button id="cancel-form" type="button">Cancel</button>
                             <button id="submit-form" type="submit">Submit</button>
                         </div>
+                        </div>
                      </form>
                    
                 </div>
-            </div>`
+            `
 }
